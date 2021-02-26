da0842879e27271fa932b926fe6eeb58e3896a84
nehakumarisaa <67169404+nehakumarisaa@users.noreply.github.com>
2021-02-26 20:30:29 +0530

    Create CodeCommentController.js

---
+++ Aura/CodeComment/CodeCommentController.js
@@ -0,0 +1,15 @@
+/**
+ * @File Name          : CodeCommentController.js
+ * @Description        : [Story/Def #] This is a sample code for code comment
+ * @Author             : Neha Kumari
+ * @Created On         : 26/02/2021
+ * @Modification Log   : 
+ * Ver       Date            Author            Modification
+ * 1.0       26/02/2021      Neha Kumari       Initial Version
+ * 2.0       28/02/2021      Selvam            Added logic for Account Save
+**/
+({
+    myAction : function(component, event, helper) {
+
+    }
+})
