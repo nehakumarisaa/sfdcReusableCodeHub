430a8073464395b1674d6d4543affb71d08df8dd
nehakumarisaa <67169404+nehakumarisaa@users.noreply.github.com>
2021-02-26 20:02:06 +0530

    Create codeComment.js

---
+++ LWC/Code Comment/codeComment.js
@@ -0,0 +1,13 @@
+/**
+ * @File Name          : codeComment.js
+ * @Description        : [Story/Def #] This is a sample code for code comment
+ * @Author             : Neha Kumari
+ * @Created On         : 26/02/2021
+ * @Modification Log   : 
+ * Ver       Date            Author            Modification
+ * 1.0       26/02/2021      Neha Kumari       Initial Version
+ * 2.0       28/02/2021      Selvam            Added logic for Account Save
+**/
+import { LightningElement } from 'lwc';
+
+export default class CodeComment extends LightningElement {}
